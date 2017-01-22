41cccbea297608a54e6993a853b10f22e7b32a1e
Ralina-s <ralina.s@yandex.ru>
2017-01-22 23:09:45 +0400

    Established synchronization

--- p6/client.cpp
+++ p6/client.cpp
@@ -7,11 +7,12 @@ Client::Client() {
     addr.sin_addr.s_addr = htons(INADDR_ANY);
 
     client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    int optval = 1;
-    setsockopt(client, 6, SO_REUSEADDR, &optval, sizeof(optval));
+    // int optval = 1;
+    // setsockopt(client, 6, SO_REUSEADDR, &optval, sizeof(optval));
     if (is_invalid(connect(client, (struct sockaddr *) &addr, sizeof(addr)))) {
         exit(-1);
     }
+    set_nonblock(client);
 
     poll_fd.fd = client;
     poll_fd.events = POLLIN;
@@ -27,7 +28,8 @@ void Client::receive() {
         sleep(1);
         poll(&poll_fd, 1, 1);
         if (poll_fd.revents & poll_fd.events) {
-            int mes_len = recv(poll_fd.fd, buf, sizeof(buf), MSG_NOSIGNAL);
+                // std::cout << "client: получила событие, которое ждала" << std::endl << std::flush;
+            int mes_len = recv(poll_fd.fd, buf, BUF_SIZE, 0);
             if (mes_len <= 0) {
                 shutdown(client, SHUT_RDWR);
                 close(client);
@@ -45,18 +47,21 @@ int Client::set(std::string key, std::string value, int ttl) {
     memcpy(buf + 1 + KEY_SIZE, value.c_str(), VALUE_SIZE);
     memcpy(buf + 1 + KEY_SIZE + VALUE_SIZE, &ttl, TTL_SIZE);
 
-    if (is_invalid(send(poll_fd.fd, buf, strlen(buf), MSG_NOSIGNAL))) {
+    if (is_invalid(send(poll_fd.fd, buf, BUF_SIZE, 0))) {
+            // std::cout << "client: не получилось отправить" << std::endl << std::flush;
         return -1;
     }   
 
+    // std::cout << "client: перед ресив" << std::endl << std::flush;
     receive();
+    // std::cout << "client: после ресив" << std::endl << std::flush;
 
     printf("%s\n", buf + 1);
     fflush(stdout);
     if (buf[0] == ERROR) {
         return -1;
     }
-    memset(buf, 0, sizeof(buf));
+    memset(buf, 0, BUF_SIZE);
     return 0;
 }
 
@@ -76,7 +81,7 @@ int Client::get(std::string key, std::string return_value) {
     if (buf[0] == ERROR) {
         return -1;
     }
-    memset(buf, 0, sizeof(buf));
+    memset(buf, 0, BUF_SIZE);
     return 0;
 }
 
@@ -96,6 +101,6 @@ int Client::delete_key(std::string key) {
     if (buf[0] == ERROR) {
         return -1;
     }
-    memset(buf, 0, sizeof(buf));
+    memset(buf, 0, BUF_SIZE);
     return 0;
 }
